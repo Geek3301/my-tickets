f1dd4916bf23cd4a13504ed3686b9df092ccbe21
fedir.bereza <fedir.bereza@check24.de>
2025-10-30 10:37:33 +0100

    changed routing (now every route starts from /tasks), added createTask updateTask and deleteTask functions

--- src/main/java/learning/reservation_system/TaskController.java
+++ src/main/java/learning/reservation_system/TaskController.java
@@ -1,15 +1,26 @@
 package learning.reservation_system;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.function.EntityResponse;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/tasks")
 
 public class TaskController {
 
@@ -22,15 +33,40 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @GetMapping("task/{id}")
+    @GetMapping
+    public List<Task> getAllTasks(){
+        log.info("Tasks were found");
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
     public Task getTaskById(@PathVariable Long id){
         log.info("Task with id " + id + " was found");
         return taskService.getTaskById(id);
     }
 
-    @GetMapping("/tasks")
-    public List<Task> getAllTasks(){
-        log.info("Tasks were found");
-        return taskService.getAllTasks();
+    @PostMapping
+    public ResponseEntity<Task> createTask(
+            @RequestBody Task newRequestBodyTask
+    ){
+        log.info("Task created");
+        return this.taskService.createTask(newRequestBodyTask);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(
+            @RequestBody Task newRequestBodyTask,
+            @PathVariable Long id
+    ){
+        log.info("Task updated");
+        return this.taskService.updateTask(id, newRequestBodyTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Task> deleteTask(
+            @PathVariable Long id
+    ){
+        log.info("Task deleted");
+        return this.taskService.deleteTask(id);
     }
 }
--- src/main/java/learning/reservation_system/TaskService.java
+++ src/main/java/learning/reservation_system/TaskService.java
@@ -1,61 +1,75 @@
 package learning.reservation_system;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 
 public class TaskService {
 
-    Map<Long, Task> tasksMap = Map.of
-            (
-            1L, new Task(
-                    1L,
-                    7L,
-                    7L,
-                    Status.DONE,
-                    LocalDateTime.now(),
-                    LocalDateTime.now().plusDays(5),
-                    Priority.Medium
-            ),
-            2L, new Task(
-                    2L,
-                    7L,
-                    7L,
-                    Status.IN_PROGRESS,
-                    LocalDateTime.now(),
-                    LocalDateTime.now().plusDays(12),
-                    Priority.High
-            ),
-            3L, new Task(
-                    3L,
-                    7L,
-                    7L,
-                    Status.CREATED,
-                    LocalDateTime.now(),
-                    LocalDateTime.now().plusDays(40),
-                    Priority.Low
-            )
-    );
+    private final Map<Long, Task> tasks;
+    private final AtomicLong taskIds;
 
-    HashMap<Long, Task> tasks = new HashMap<>(tasksMap);
+    public TaskService(){
+        tasks = new HashMap<>();
+        taskIds = new AtomicLong(0L);
+    }
+
 
     public Task getTaskById(Long id)
     {
         return tasks.get(id);
     }
 
-    public List<Task> getAllTasks()
-    {
-        return List.of
-                (
-                tasks.get(1L),
-                tasks.get(2L),
-                tasks.get(3L)
-                );
+    public List<Task> getAllTasks() {
+        return new ArrayList<>(tasks.values());
+    }
+
+
+    public ResponseEntity<Task> createTask(Task newRequestBodyTask) {
+    Task newTask = new Task(
+            taskIds.incrementAndGet(),
+            newRequestBodyTask.creatorId(),
+            newRequestBodyTask.assignedUserId(),
+            newRequestBodyTask.status(),
+            newRequestBodyTask.createDateTime(),
+            newRequestBodyTask.deadlineDate(),
+            Priority.Low
+    );
+    tasks.put(newTask.id(), newTask);
+    return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(newTask);
+    }
+
+    public ResponseEntity<Task> updateTask(Long id, Task newRequestBodyTask) {
+        Task newTask = new Task(
+                id,
+                newRequestBodyTask.creatorId(),
+                newRequestBodyTask.assignedUserId(),
+                newRequestBodyTask.status(),
+                newRequestBodyTask.createDateTime(),
+                newRequestBodyTask.deadlineDate(),
+                newRequestBodyTask.priority()
+        );
+        tasks.put(id, newTask);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(newTask);
+    }
+
+    public ResponseEntity<Task> deleteTask(Long id) {
+        ResponseEntity<Task> response = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(tasks.get(id));
+        tasks.remove(id);
+        return response;
     }
 }
